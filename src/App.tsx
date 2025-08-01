9a6980a9ded0ed04726486b3ad37ce12894f31c0
Cristian Velandia <cvelandia+tsco@TractorSupply.com>
2025-08-01 12:50:24 -0500

    Implement basic games fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
-import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
 
 function App() {
   return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main" `,
-        lg: `"nav nav" "aside main"`,
-      }}
-    >
-      <GridItem area="nav">
-        <NavBar></NavBar>
-      </GridItem>
-      <Show above="lg">
-        <GridItem area="aside" bg="gold">
-          Aside
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main" `,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav">
+          <NavBar></NavBar>
         </GridItem>
-      </Show>
-      <GridItem area="main" bg="dodgerblue">
-        Main
-      </GridItem>
-    </Grid>
+        <Show above="lg">
+          <GridItem area="aside">Aside</GridItem>
+        </Show>
+        <GridItem area="main">
+          <GameGrid />
+        </GridItem>
+      </Grid>
+    </>
   );
 }
 
