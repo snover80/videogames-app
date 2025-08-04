c6be90693c157205f77e6c43f369151d04de3f24
Cristian Velandia <cvelandia+tsco@TractorSupply.com>
2025-08-04 15:18:56 -0500

    Adding statick data

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,13 +1,13 @@
-import useData from "./useData";
+import genres from "../data/genres";
 
 export interface Genre {
-  id: string;
+  id: number;
   name: string;
   slug: string;
   games_count: number;
   image_background: string;
 }
 
-const useGenre = () => useData<Genre>("/genres");
+const useGenre = () => ({ data: genres, isLoading: false, error: null });
 
 export default useGenre;
