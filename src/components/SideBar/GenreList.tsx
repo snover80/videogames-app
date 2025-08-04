3eff8eef4602aab8bc46c82225739c52e22001f4
Cristian Velandia <cvelandia+tsco@TractorSupply.com>
2025-08-04 09:22:30 -0500

    Generic api and generic hook

--- src/components/SideBar/GenreList.tsx
+++ src/components/SideBar/GenreList.tsx
@@ -1,10 +1,10 @@
 import useGenre from "../../hooks/useGenre";
 
 function GenreList() {
-  const { genres } = useGenre();
+  const { data } = useGenre();
   return (
     <ul>
-      {genres.map((genre) => (
+      {data.map((genre) => (
         <li key={genre.id}>{genre.name}</li>
       ))}
     </ul>
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://api.rawg.io/api",
-    params: {
-        key: "0a668e1f6ef3454faeb9862357fb60ab"
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "0a668e1f6ef3454faeb9862357fb60ab",
+  },
+});
