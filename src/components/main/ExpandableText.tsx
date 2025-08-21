4b29eb84ae68f1848da2a2922a9661abfdfe0761
Cristian Velandia <cvelandia+tsco@TractorSupply.com>
2025-08-21 10:30:04 -0500

    Building expandable text

---
+++ src/components/main/ExpandableText.tsx
@@ -0,0 +1,31 @@
+import { Button, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+interface ExpandableTextProps {
+  children: string;
+}
+function ExpandableText({ children }: ExpandableTextProps) {
+  const [expanded, setExapnded] = useState(false);
+  const limit = 300;
+
+  if (!children) return null;
+  if (children.length <= limit) return <Text>{children}</Text>;
+
+  const summary = expanded ? children : children.substring(0, limit) + "...";
+  return (
+    <Text>
+      {summary}
+      <Button
+        marginLeft={1}
+        size="xs"
+        fontWeight="bold"
+        colorScheme="yellow"
+        onClick={() => setExapnded(!expanded)}
+      >
+        {expanded ? "Show Less" : "Show More"}
+      </Button>
+    </Text>
+  );
+}
+
+export default ExpandableText;
